7e92150d8d27832c72323ddb8cd2d3445f1ccb29
Joshua Peek <josh@joshpeek.com>
2015-05-12 14:06:44 -0700

    Move tests to fixture files

--- test/test_editor_config.rb
+++ test/test_editor_config.rb
@@ -38,84 +38,92 @@ class TestEditorConfig < MiniTest::Test
   end
 
   def test_preprocess_indent_style
+    config, _ = EditorConfig.parse(fixture(:indent_style))
     assert_equal({ "indent_style" => "tab" },
-      EditorConfig.preprocess({ "indent_style" => "tab" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({ "indent_style" => "space" },
-      EditorConfig.preprocess({ "indent_style" => "space" }))
+      EditorConfig.preprocess(config["test2"]))
     assert_equal({ "indent_style" => "space" },
-      EditorConfig.preprocess({ "indent_style" => "SPACE" }))
+      EditorConfig.preprocess(config["test3"]))
     assert_equal({},
-      EditorConfig.preprocess({ "indent_style" => "emoji" }))
+      EditorConfig.preprocess(config["test4"]))
   end
 
   def test_preprocess_indent_size
+    config, _ = EditorConfig.parse(fixture(:indent_size))
     assert_equal({ "indent_size" => "tab" },
-      EditorConfig.preprocess({ "indent_size" => "tab" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({ "indent_size" => 4 },
-      EditorConfig.preprocess({ "indent_size" => "4" }))
+      EditorConfig.preprocess(config["test2"]))
     assert_equal({},
-      EditorConfig.preprocess({ "indent_size" => "0" }))
+      EditorConfig.preprocess(config["test3"]))
     assert_equal({},
-      EditorConfig.preprocess({ "indent_size" => "space" }))
+      EditorConfig.preprocess(config["test4"]))
   end
 
   def test_preprocess_tab_width
+    config, _ = EditorConfig.parse(fixture(:tab_width))
     assert_equal({ "tab_width" => 4 },
-      EditorConfig.preprocess({ "tab_width" => "4" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({},
-      EditorConfig.preprocess({ "tab_width" => "0" }))
+      EditorConfig.preprocess(config["test2"]))
   end
 
   def test_preprocess_end_of_line
+    config, _ = EditorConfig.parse(fixture(:end_of_line))
     assert_equal({ "end_of_line" => "lf" },
-      EditorConfig.preprocess({ "end_of_line" => "lf" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({ "end_of_line" => "crlf" },
-      EditorConfig.preprocess({ "end_of_line" => "crlf" }))
+      EditorConfig.preprocess(config["test2"]))
     assert_equal({ "end_of_line" => "cr" },
-      EditorConfig.preprocess({ "end_of_line" => "cr" }))
+      EditorConfig.preprocess(config["test3"]))
     assert_equal({ "end_of_line" => "crlf" },
-      EditorConfig.preprocess({ "end_of_line" => "CRLF" }))
+      EditorConfig.preprocess(config["test4"]))
   end
 
   def test_preprocess_charset
+    config, _ = EditorConfig.parse(fixture(:charset))
     assert_equal({ "charset" => "latin1" },
-      EditorConfig.preprocess({ "charset" => "latin1" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({ "charset" => "utf-8" },
-      EditorConfig.preprocess({ "charset" => "utf-8" }))
+      EditorConfig.preprocess(config["test2"]))
     assert_equal({ "charset" => "utf-16be" },
-      EditorConfig.preprocess({ "charset" => "utf-16be" }))
+      EditorConfig.preprocess(config["test3"]))
     assert_equal({ "charset" => "utf-16le" },
-      EditorConfig.preprocess({ "charset" => "utf-16le" }))
+      EditorConfig.preprocess(config["test4"]))
     assert_equal({ "charset" => "utf-8" },
-      EditorConfig.preprocess({ "charset" => "UTF-8" }))
+      EditorConfig.preprocess(config["test5"]))
   end
 
   def test_trim_trailing_whitespace
+    config, _ = EditorConfig.parse(fixture(:trim_trailing_whitespace))
     assert_equal({ "trim_trailing_whitespace" => true },
-      EditorConfig.preprocess({ "trim_trailing_whitespace" => "true" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({ "trim_trailing_whitespace" => false },
-      EditorConfig.preprocess({ "trim_trailing_whitespace" => "false" }))
+      EditorConfig.preprocess(config["test2"]))
     assert_equal({ "trim_trailing_whitespace" => true },
-      EditorConfig.preprocess({ "trim_trailing_whitespace" => "TRUE" }))
+      EditorConfig.preprocess(config["test3"]))
     assert_equal({},
-      EditorConfig.preprocess({ "trim_trailing_whitespace" => "nil" }))
+      EditorConfig.preprocess(config["test4"]))
   end
 
   def test_insert_final_newline
+    config, _ = EditorConfig.parse(fixture(:insert_final_newline))
     assert_equal({ "insert_final_newline" => true },
-      EditorConfig.preprocess({ "insert_final_newline" => "true" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({ "insert_final_newline" => false },
-      EditorConfig.preprocess({ "insert_final_newline" => "false" }))
+      EditorConfig.preprocess(config["test2"]))
     assert_equal({ "insert_final_newline" => true },
-      EditorConfig.preprocess({ "insert_final_newline" => "TRUE" }))
+      EditorConfig.preprocess(config["test3"]))
     assert_equal({},
-      EditorConfig.preprocess({ "insert_final_newline" => "nil" }))
+      EditorConfig.preprocess(config["test4"]))
   end
 
   def test_max_line_length
+    config, _ = EditorConfig.parse(fixture(:max_line_length))
     assert_equal({ "max_line_length" => 80 },
-      EditorConfig.preprocess({ "max_line_length" => "80" }))
+      EditorConfig.preprocess(config["test1"]))
     assert_equal({},
-      EditorConfig.preprocess({ "max_line_length" => "0" }))
+      EditorConfig.preprocess(config["test2"]))
   end
 end
