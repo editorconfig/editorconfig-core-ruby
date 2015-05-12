15751567747ec3d22b60d6c96b8a695e6d7d011f
Joshua Peek <josh@joshpeek.com>
2015-05-12 13:52:40 -0700

    Create fixtures test dir

--- test/test_editor_config.rb
+++ test/test_editor_config.rb
@@ -2,43 +2,12 @@ require "minitest/autorun"
 require "editor_config"
 
 class TestEditorConfig < MiniTest::Test
+  def fixture(name)
+    path = File.join(__dir__, "fixtures/#{name}")
+    File.read(path)
+  end
+
   def test_parse_example
-    config = <<-EOS.gsub(/^      /, "")
-      # EditorConfig is awesome: http://EditorConfig.org
-
-      # top-most EditorConfig file
-      root = true
-
-      # Unix-style newlines with a newline ending every file
-      [*]
-      end_of_line = lf
-      insert_final_newline = true
-
-      # Matches multiple files with brace expansion notation
-      # Set default charset
-      [*.{js,py}]
-      charset = utf-8
-
-      # 4 space indentation
-      [*.py]
-      indent_style = space
-      indent_size = 4
-
-      # Tab indentation (no size specified)
-      [Makefile]
-      indent_style = tab
-
-      # Indentation override for all JS under lib directory
-      [lib/**.js]
-      indent_style = space
-      indent_size = 2
-
-      # Matches the exact files either package.json or .travis.yml
-      [{package.json,.travis.yml}]
-      indent_style = space
-      indent_size = 2
-    EOS
-
     assert_equal([
       {
         "*" => {
@@ -65,7 +34,7 @@ class TestEditorConfig < MiniTest::Test
         }
       },
       true
-    ], EditorConfig.parse(config))
+    ], EditorConfig.parse(fixture(:sample)))
   end
 
   def test_preprocess_indent_style
